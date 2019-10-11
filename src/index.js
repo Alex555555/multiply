6d38fcceead07503fcf41f6dd38589958061879d
Oleksii Suhetskyi <oleksiy.s228s@gmail.com>
2019-10-11 21:44:22 +0300

    Base structure

---
+++ src/index.js
@@ -0,0 +1,36 @@
+module.exports = function multiply(first, second) {
+ 
+  let arrRFirst = first.split("").reverse();
+  let arrRSecond = second.split("").reverse();
+
+  let stack = [];
+
+  for (let i = 0; i < arrRFirst.length; i++) {
+    for (let j = 0; j < arrRSecond.length; j++) {
+      let partMul = arrRFirst[i] * arrRSecond[j];
+
+      if (stack[i + j] !== undefined) {
+        stack[i + j] += partMul;
+      } else {
+        stack[i + j] = partMul;
+      }
+    }
+  }
+
+  
+  for (let i = 0; i < stack.length; i++) {
+    let num = stack[i] % 10;
+    let head = Math.floor(stack[i] / 10);
+    stack[i] = num;
+
+    
+    if (stack[i + 1] !== undefined) {
+      stack[i + 1] += head;
+    } else if (head != 0) {
+      stack[i + 1] = head;
+    }
+  }
+  return stack.reverse().join("");
+
+
+}
